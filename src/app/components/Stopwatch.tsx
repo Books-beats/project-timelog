69bff9b0f304c8d2d9e0901dd40800d2a891158a
Abhishek Verma <wigeriaaeriag@gmail.com>
2025-03-31 03:11:45 +0000

    Fixes

--- src/app/components/Stopwatch.tsx
+++ src/app/components/Stopwatch.tsx
@@ -18,31 +18,57 @@ const Stopwatch = ({
     removeProject(id);
   };
 
-  let startTime;
+  const [startDateString, setStartDateString] = useState<string>('');
   let elapsedPausedTime = 0;
-  const today = new Date();
   // console.log(today);
   const [stopwatchInterval, setStopwatchInterval] = useState<any>();
   const [isRunning, setIsRunning] = useState(false);
 
   function startStopwatch() {
+    const today = new Date();
     let timeParts = timeElapsed.split(":");
+    timeParts = [
+      today.getHours() - timeParts[0],
+      today.getMinutes() - timeParts[1],
+      today.getSeconds() - timeParts[2],
+    ]
     console.log(timeParts);
     let date = new Date(
       today.getFullYear(),
       today.getMonth(),
       today.getDate(),
-      Number(timeParts[0]),
-      Number(timeParts[1]),
-      Number(timeParts[2])
+      timeParts[0],
+      timeParts[1],
+      timeParts[2],
     );
-    // console.log(date);
-    if (!isRunning) {
-      let setinterval = setInterval(updateStopwatch, 1000);
-      startTime = date.getTime() - elapsedPausedTime;
-      setStopwatchInterval(setinterval);
-      setIsRunning(true);
+    setStartDateString(date.toISOString())
+    console.log("Setting start date", startDateString, date)
+
+    function updateStopwatch() {
+      const currentDate = new Date();
+      var currentTime = currentDate.getTime();
+      var elapsedTime = currentTime - date.getTime();
+      console.log({ elapsedTime, currentDate, date })
+      var seconds = Math.floor(elapsedTime / 1000) % 60;
+      var minutes = Math.floor(elapsedTime / 1000 / 60) % 60;
+      var hours = Math.floor(elapsedTime / 1000 / 60 / 60);
+      var displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+      updateProject(displayTime, id);
+      // updateElapsedTime(displayTime, id);
+      document.getElementById(`${id}`).innerHTML = displayTime;
     }
+  
+    let interval = setInterval(updateStopwatch, 1000);
+    setStopwatchInterval(interval)
+    setIsRunning(true)
+    // console.log(date);
+    // if (!isRunning) {
+    //   let setinterval = setInterval(updateStopwatch, 1000);
+    //   startTime = date.getTime() - elapsedPausedTime;
+    //   console.log({ startTime, currentTime: date, pauseTime: elapsedPausedTime, })
+    //   setStopwatchInterval(setinterval);
+    //   setIsRunning(true);
+    // }
     // console.log("start", isRunning);
   }
 
@@ -51,34 +77,23 @@ const Stopwatch = ({
     // console.log("isRunning", isRunning);
     if (isRunning) {
       clearInterval(stopwatchInterval);
-      elapsedPausedTime = new Date().getTime() - startTime;
+      elapsedPausedTime = new Date().getTime() - new Date(startDateString).getTime();
       setIsRunning(false);
     }
   }
 
   function stopStopwatch() {
     clearInterval(stopwatchInterval);
-    elapsedPausedTime = new Date().getTime() - startTime;
+    elapsedPausedTime = new Date().getTime() - new Date(startDateString).getTime();
     setIsRunning(false);
     elapsedPausedTime = 0;
     document.getElementById(`${id}`).innerHTML = "00:00:00";
     console.log("stop");
+    updateProject("00:00:00", id)
   }
 
   function resetStopwatch() {
     stopStopwatch();
-  }
-
-  function updateStopwatch() {
-    var currentTime = new Date().getTime();
-    var elapsedTime = currentTime - startTime;
-    var seconds = Math.floor(elapsedTime / 1000) % 60;
-    var minutes = Math.floor(elapsedTime / 1000 / 60) % 60;
-    var hours = Math.floor(elapsedTime / 1000 / 60 / 60);
-    var displayTime = pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
-    updateProject(displayTime, id);
-    // updateElapsedTime(displayTime, id);
-    document.getElementById(`${id}`).innerHTML = displayTime;
   }
 
   function pad(number) {
